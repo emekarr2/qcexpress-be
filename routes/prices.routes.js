45e6ffa89e0f0ee8e234b0402f92b5afb0dc5e44
gospel4me <tracy@gloqal.co>
2022-05-16 12:47:29 +0100

    m

--- routes/prices.routes.js
+++ routes/prices.routes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const zonedata = require("./prices");
 const contries = require("./countries");
+const importdata = require("./import");
 
 let newtotal;
+
 const calculatePrice = (weight, zone) => {
   let rate;
   let price;
@@ -280,6 +282,200 @@ const calculateShipping = (country, weight, type) => {
   }
 };
 
+
+const calculateImpPrice = (weight, zone) => {
+  let rate;
+  let price;
+  console.log(weight, zone);
+  let srate = zonedata.find((data) => data.weight == weight);
+  console.log(srate, "srate");
+  switch (zone.Zone) {
+    case "zone1":
+      rate = srate.zone1.rate;
+      price = parseInt(srate.zone1.price);
+      var margin = rate * price;
+      var wrate = margin * 0.075;
+      newtotal = price + wrate + margin;
+      return newtotal;
+      break;
+    case "zone2":
+      rate = srate.zone2.rate;
+      price = srate.zone2.price;
+      var margin = rate * price;
+      var wrate = margin * 0.075;
+      newtotal = price + wrate + margin;
+      return newtotal;
+      break;
+    case "zone3":
+      rate = srate.zone3.rate;
+      price = srate.zone3.price;
+      var margin = rate * price;
+      var wrate = margin * 0.075;
+      newtotal = price + wrate + margin;
+      return newtotal;
+      break;
+    case "zone4":
+      rate = srate.zone4.rate;
+      price = parseInt(srate.zone4.price);
+      var margin = rate * price;
+      var wrate = margin * 0.075;
+      newtotal = price + wrate + margin;
+      return newtotal;
+      break;
+    case "zone5":
+      rate = srate.zone5.rate;
+      price = srate.zone5.price;
+      var margin = rate * price;
+      var wrate = margin * 0.075;
+      newtotal = price + wrate + margin;
+      return newtotal;
+      break;
+    case "zone6":
+      rate = srate.zone6.rate;
+      price = srate.zone6.price;
+      var margin = rate * price;
+      var wrate = margin * 0.075;
+      newtotal = price + wrate + margin;
+      return newtotal;
+      break;
+    case "zone7":
+      rate = srate.zone7.rate;
+      price = srate.zone7.price;
+      var margin = rate * price;
+      var wrate = margin * 0.075;
+      newtotal = price + wrate + margin;
+      return newtotal;
+      break;
+    case "zone8":
+      rate = srate.zone8.rate;
+      price = srate.zone8.price;
+      var margin = rate * price;
+      var wrate = margin * 0.075;
+      newtotal = price + wrate + margin;
+      console.log(newtotal,"kk")
+      return newtotal;
+      break;
+  }
+};
+
+const calculateImpShipping = (country, weight, type) => {
+  let zone = contries.find((zone) => zone.Countries === country);
+  const newZone = zone.Zone;
+  weight = parseFloat(weight);
+  console.log(weight)
+  if (type == "Document" && weight <= 2) {
+    console.log(type,country,newZone)
+
+    if (weight <= 0.5) {
+      switch (newZone) {
+        case "zone1":
+          var margin = 0.25 * 11256.3;
+          var rate = margin * 0.075;
+          newtotal = 11256.3 + margin + rate;
+          return newtotal;
+          break;
+        case "zone2":
+          var margin = 0.25 * 12599.87;
+          var rate = margin * 0.075;
+          newtotal = 12599.87 + rate + margin;
+          return newtotal;
+          break;
+        case "zone3":
+          var margin = 0.25 * 13943.43;
+          var rate = margin * 0.075;
+          newtotal = 13943.43 + margin + rate;
+          return newtotal;
+          break;
+        case "zone4":
+          var margin = 0.25 * 16182.71;
+          var rate = margin * 0.075;
+          newtotal = 16182.71 + rate + margin;
+          return newtotal;
+          break;
+        case "zone5":
+          var margin = 0.25 * 16630.55;
+          var rate = margin * 0.075;
+          newtotal = 16630.55 + rate + margin;
+          return newtotal;
+          break;
+        case "zone6":
+          var margin = 0.25 * 17974.11;
+          var rate = margin * 0.075;
+          newtotal = 17974.11 + rate + margin;
+          return newtotal;
+          break;
+        case "zone7":
+          var margin = 0.25 * 20307.62;
+          var rate = margin * 0.075;
+          newtotal = 20307.62 + rate + margin;
+          setTotal(newtotal);
+          break;
+        case "zone8":
+          var margin = 0.25 * 21203.34;
+          var rate = margin * 0.075;
+          newtotal = 21203.34 + rate + margin;
+          console.log(newtotal)
+          return newtotal;
+          break;
+      }
+    } else if (weight <= 1) {
+      switch (newZone) {
+        case "zone1":
+          var margin = 0.25 * 11316.26;
+          var rate = margin * 0.075;
+          newtotal = 11316.26 + rate + margin;
+          return newtotal;
+          break;
+        case "zone2":
+          var margin = 0.25 * 12659.83;
+          var rate = margin * 0.075;
+          newtotal = 12659.83 + rate + margin;
+          return newtotal;
+          break;
+        case "zone3":
+          var margin = 0.25 * 14003.39;
+          var rate = margin * 0.075;
+          newtotal = 14003.39 + rate + margin;
+          return newtotal;
+          break;
+        case "zone4":
+          var margin = 0.25 * 16242.67;
+          var rate = margin * 0.075;
+          newtotal = 16242.67 + rate + margin;
+          return newtotal;
+          break;
+        case "zone5":
+          var margin = 0.25 * 16690.51;
+          var rate = margin * 0.075;
+          newtotal = 16690.51 + rate + margin;
+          return newtotal;
+          break;
+        case "zone6":
+          var margin = 0.25 * 18034.07;
+          var rate = margin * 0.075;
+          newtotal = 18034.07 + rate + margin;
+          return newtotal;
+          break;
+        case "zone7":
+          var margin = 0.25 * 20909.67;
+          var rate = margin * 0.075;
+          newtotal = 20909.67 + rate + margin;
+          return newtotal;
+          break;
+
+        case "zone8":
+          var margin = 0.25 * 21805.4;
+          var rate = margin * 0.075;
+          newtotal = 21805.4 + rate + margin;
+          return newtotal;
+          break;
+      }
+    }
+  } else {
+    calculateImpPrice(weight, zone);
+  }
+};
+
 router.post("/", (req, res, next) => {
   const { weight, zone, type, country } = req.body;
   if (type == "Document" && weight <= 2) {
@@ -290,6 +486,23 @@ router.post("/", (req, res, next) => {
     });
   } else {
     var message = calculatePrice(weight, zone);
+    res.status(201).json({
+      message: "success",
+      result: Math.round(message),
+    });
+  }
+});
+
+router.post("/import", (req, res, next) => {
+  const { weight, zone, type, country } = req.body;
+  if (type == "Document" && weight <= 2) {
+    var message = calculateImpShipping(country, weight, type);
+    res.status(201).json({
+      message: "success",
+      result: Math.round(message),
+    });
+  } else {
+    var message = calculateImpPrice(weight, zone);
     res.status(201).json({
       message: "success",
       result: Math.round(message),
