2db4da67c721b09f20bfa4cbc44de48a42a48324
emekarr <aceanyanwu02@gmail.com>
2022-07-22 02:24:29 +0100

    set up ttl for otp

--- app/authentication/models/Otp.js
+++ app/authentication/models/Otp.js
@@ -1,16 +1,21 @@
 const { Schema, model } = require('mongoose');
 
-const OtpSchema = new Schema({
-	email: {
-		type: String,
-		unique: true,
-		required: true,
-		index: true,
+const OtpSchema = new Schema(
+	{
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+			index: true,
+		},
+		code: {
+			type: String,
+			required: true,
+		},
 	},
-	code: {
-		type: String,
-		required: true,
-	},
-});
+	{ timestamps: true },
+);
+
+OtpSchema.index({ createdAt: 1 }, { expires: '120s' });
 
 module.exports = model('Otp', OtpSchema);
