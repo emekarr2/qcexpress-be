3d02ebbb6cd8cb5dea9562f0f2198c5a7077a98a
emekarr <aceanyanwu02@gmail.com>
2022-08-05 11:07:48 +0100

    fixed auth middleware issue

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,27 +1,34 @@
 const ServerResponse = require('../utils/response');
 const token = require('../app/authentication/tokens');
+const CustomError = require('../errors/error');
 
 module.exports = async (req, res, next) => {
-	const tokenHeader = req.headers.authorization;
-	if (typeof tokenHeader === 'undefined')
-		return ServerResponse.message('an access token is required for this route')
-			.success(false)
-			.statusCode(403)
-			.respond(res);
-	const bearer = tokenHeader.split(' ')[1];
-	const result = await token.verifyToken(bearer);
-	if (result.type != token.TOKEN_TYPE.ACCESS_TOKEN)
-		return ServerResponse.message('invalid access token used')
-			.success(false)
-			.statusCode(403)
-			.respond(res);
-	req.user = {};
-	req.user.email = result.email;
-	req.user.userId = result.userId;
-	req.user.username = result.username;
-	req.user.firstname = result.firstname;
-	req.user.lastname = result.lastname;
-	req.user.verified_email = result.verified_email;
-	req.user.verified_mobile = result.verified_mobile;
-	next();
+	try {
+		const tokenHeader = req.headers.authorization;
+		if (typeof tokenHeader === 'undefined')
+			return ServerResponse.message(
+				'an access token is required for this route',
+			)
+				.success(false)
+				.statusCode(403)
+				.respond(res);
+		const bearer = tokenHeader.split(' ')[1];
+		const result = await token.verifyToken(bearer);
+		if (result.type != token.TOKEN_TYPE.ACCESS_TOKEN)
+			return ServerResponse.message('invalid access token used')
+				.success(false)
+				.statusCode(403)
+				.respond(res);
+		req.user = {};
+		req.user.email = result.email;
+		req.user.userId = result.userId;
+		req.user.username = result.username;
+		req.user.firstname = result.firstname;
+		req.user.lastname = result.lastname;
+		req.user.verified_email = result.verified_email;
+		req.user.verified_mobile = result.verified_mobile;
+		next();
+	} catch (err) {
+		next(new CustomError('access denied', 403));
+	}
 };
