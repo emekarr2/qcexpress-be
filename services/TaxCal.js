7c5b7be8b4c76d7a2f64a1cbe3abf16388f805c3
emekarr <aceanyanwu02@gmail.com>
2022-10-04 11:50:18 +0100

    tax calculator

---
+++ services/TaxCal.js
@@ -0,0 +1,24 @@
+const vat = require('../constants/vat');
+const zone_rates = require('../constants/zone_rates');
+const zones = require('../constants/zones');
+const CustomError = require('../errors/error');
+
+module.exports = (amount, document, weight, destination, deliveryType) => {
+	if (destination === 'NG' || deliveryType === 'domestic') {
+		return amount;
+	}
+	const zone = zones.find(
+		(z) =>
+			z.Country.substring(
+				z.Country.indexOf('(') + 1,
+				z.Country.lastIndexOf(')'),
+			) === destination,
+	);
+	const charge = zone_rates[deliveryType][zone.Zone][document].find(
+		(c) => weight >= c.min && weight <= c.max,
+	);
+	if (!charge) throw new CustomError(`unsupported weight size selected`, 401);
+	const priceBeforeVat = (charge.charge * amount) / 100 + amount;
+	const priceAfterVat = (priceBeforeVat * vat) / 100 + priceBeforeVat;
+	return priceAfterVat;
+};
