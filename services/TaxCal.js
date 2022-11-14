ae5a272b8a2f55113435f4ff80f32a98b50419b5
emekarr <aceanyanwu02@gmail.com>
2022-11-14 10:59:52 +0100

    fixed pricing

--- services/TaxCal.js
+++ services/TaxCal.js
@@ -1,24 +1,25 @@
-const vat = require('../constants/vat');
-const zone_rates = require('../constants/zone_rates');
-const zones = require('../constants/zones');
-const CustomError = require('../errors/error');
+const vat = require("../constants/vat");
+const zone_rates = require("../constants/zone_rates");
+const zones = require("../constants/zones");
+const CustomError = require("../errors/error");
 
 module.exports = (amount, document, weight, destination, deliveryType) => {
-	if (destination === 'NG' || deliveryType === 'domestic') {
-		return amount;
-	}
-	const zone = zones.find(
-		(z) =>
-			z.Country.substring(
-				z.Country.indexOf('(') + 1,
-				z.Country.lastIndexOf(')'),
-			) === destination,
-	);
-	const charge = zone_rates[deliveryType][zone.Zone][document].find(
-		(c) => weight >= c.min && weight <= c.max,
-	);
-	if (!charge) throw new CustomError(`unsupported weight size selected`, 401);
-	const priceBeforeVat = (charge.charge * amount) / 100 + amount;
-	const priceAfterVat = (priceBeforeVat * vat) / 100 + priceBeforeVat;
-	return priceAfterVat;
+  if (destination === "NG" || deliveryType === "domestic") {
+    return amount;
+  }
+  const zone = zones.find(
+    (z) =>
+      z.Country.substring(
+        z.Country.indexOf("(") + 1,
+        z.Country.lastIndexOf(")")
+      ) === destination
+  );
+  const charge = zone_rates[deliveryType][zone.Zone][document].find(
+    (c) => weight >= c.min && weight <= c.max
+  );
+  if (!charge) throw new CustomError(`unsupported weight size selected`, 401);
+  const markup = (charge.charge * amount) / 100;
+  const markupVat = (markup * vat) / 100;
+  const price = amount + markupVat;
+  return price;
 };
