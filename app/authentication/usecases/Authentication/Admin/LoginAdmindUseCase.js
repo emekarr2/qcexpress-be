73fe75a8cda0a6d3d3a3cc9d7f5d70b4f9526f61
emekarr <aceanyanwu02@gmail.com>
2023-02-09 02:13:52 +0100

    built login admin usecase

---
+++ app/authentication/usecases/Authentication/Admin/LoginAdmindUseCase.js
@@ -0,0 +1,45 @@
+const adminRepo = require("../../../../admin/repository/admin_repo");
+const hasher = require("../../../hasher");
+const tokens = require("../../../tokens");
+const CustomError = require("../../../../../errors/error");
+
+class LoginAdminUseCase {
+  #adminRepo = adminRepo;
+
+  #hasher = hasher;
+
+  #tokens = tokens;
+
+  async execute(data) {
+    const admin = await this.#adminRepo.findOneByFields({
+      email: data.account,
+    });
+    this.#__rejectOnAdminDoesNotExist(admin);
+    const success = await this.#hasher.verifyPassword(
+      data.password,
+      admin.password
+    );
+    if (!success) throw new CustomError("incorrect password", 403);
+    const access_token = await this.#tokens.generateAccessToken(
+      user.email,
+      user.id,
+      user.name
+    );
+    const refresh_tokens = await this.#tokens.generateRefreshToken(
+      user.email,
+      user.id,
+      user.name
+    );
+    return {
+      access_token,
+      refresh_tokens,
+      admin,
+    };
+  }
+
+  #__rejectOnAdminDoesNotExist(admin) {
+    if (!admin) throw new CustomError("admin does not exist", 404);
+  }
+}
+
+module.exports = Object.freeze(new LoginAdminUseCase());
