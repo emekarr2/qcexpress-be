280b05bb40d771fb4c70892d12a0432344a990d7
Emekarr <aceanyanwu02@gmail.com>
2023-12-11 13:38:32 +0100

    new business tracking route

--- routes/v1/tracking.routes.js
+++ routes/v1/tracking.routes.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
 const TrackingController = require("../../app/tracking/controller");
 const auth_middleware = require("../../middlewares/userBusinessAuth");
+const business_admin_auth_middleware = require("../../middlewares/businessAdminAuth");
 
 const router = Router();
 
 router.get("/single", auth_middleware(false), TrackingController.trackShipment);
+
+router.get(
+  "/single/dashboard",
+  business_admin_auth_middleware("2"),
+  TrackingController.trackShipment
+);
 
 module.exports = router;
