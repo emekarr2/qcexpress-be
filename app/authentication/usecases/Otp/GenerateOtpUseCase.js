023b10342ca1a76aadf4fc8b53d24eaaa4e369b1
Emekarr <aceanyanwu02@gmail.com>
2023-12-19 11:39:47 +0100

    test

--- app/authentication/usecases/Otp/GenerateOtpUseCase.js
+++ app/authentication/usecases/Otp/GenerateOtpUseCase.js
@@ -1,20 +1,25 @@
-const generate_otp = require('../../../../utils/generate_otp');
-const otpRepo = require('../../repository/otp_repo');
-const hasher = require('../../hasher');
+const generate_otp = require("../../../../utils/generate_otp");
+const otpRepo = require("../../repository/otp_repo");
+const hasher = require("../../hasher");
 
 class GenerateOtpUseCase {
-	#otpRepo = otpRepo;
-	#generateOtp = generate_otp;
-	#hasher = hasher;
+  #otpRepo = otpRepo;
+  #generateOtp = generate_otp;
+  #hasher = hasher;
 
-	async execute(email) {
-		const code = this.#generateOtp();
-		await this.#otpRepo.upsert(
-			{ email },
-			{ email, code: await this.#hasher.hashPassword(code) },
-		);
-		return code;
-	}
+  async execute(email) {
+    const code = this.#generateOtp();
+	console.log(process.env.NODE_ENV)
+    if (process.env.NODE_ENV !== "production") {
+      console.log(code);
+    }
+    console.log("code", code);
+    await this.#otpRepo.upsert(
+      { email },
+      { email, code: await this.#hasher.hashPassword(code) }
+    );
+    return code;
+  }
 }
 
 module.exports = Object.freeze(new GenerateOtpUseCase());
