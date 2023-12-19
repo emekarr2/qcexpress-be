b390f278384e5fa06ed0bf0cc7b92953d7154c39
Emekarr <aceanyanwu02@gmail.com>
2023-12-19 11:11:18 +0100

    otp updte

--- utils/generate_otp.js
+++ utils/generate_otp.js
@@ -1,1 +1,6 @@
-module.exports = () => Math.floor(1000 + Math.random() * 8999).toString();
+module.exports = () => {
+  if (process.env.NODE_ENV !== "production") {
+    return "0000";
+  }
+  return Math.floor(1000 + Math.random() * 8999).toString();
+};
